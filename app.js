762fd0be7c02210392cae2380e37c321d978efe8
Devon Javier <devonjarekjavier@gmail.com>
2024-08-31 02:56:39 +0800

    Front-end for testing purposes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ server.engine('hbs', handlebars.engine({
 }));
 
 const mongoose = require('mongoose');
-const mongo_uri = 'mongodb://127.0.0.1:27017/r_colludium';
+const mongo_uri = 'mongodb://127.0.0.1:27017/quiz-website';
 mongoose.connect(mongo_uri);
 
 server.use(express.static('public'));
@@ -23,6 +23,12 @@ server.get('/', (req, resp) => {
         title: 'Backend Development Challenge'
     });
 });
+
+const questionSchema = new mongoose.Schema({
+    question : { type: String, required: true },
+    choices : { type: [String], required: true },
+    correct_index: { type: Number, required: true }
+}, {versionKey : false});
 
 server.listen(3000, () => {
     console.log('Listening on host port 3000');
